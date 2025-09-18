c6c0fb59d8f63e3c4187d4e6888c38b2cc18daff
Arpit Tripathi <32494300+arpit04tripathi@users.noreply.github.com>
2025-09-18 12:29:15 +0100

    Add modules (#2)
    * create new modules
    
    * add lombok and build gradle
    
    * remove default modules

---
+++ structural/src/main/java/com/a04t/dp/structural/Main.java
@@ -0,0 +1,12 @@
+package com.a04t.dp.structural;
+
+public class Main {
+    public static final String DIVIDER = "=============================================";;
+
+    public static void main(String[] args) {
+        System.out.println(DIVIDER);
+        System.out.println("Hello world!");
+        System.out.println("started Main in com.a04t.dp.structural");
+        System.out.println(DIVIDER);
+    }
+}
