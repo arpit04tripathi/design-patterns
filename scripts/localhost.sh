5022874d9d77fd4666234b3d858644124a0030b3
Arpit Tripathi <arpit04tripathi@gmail.com>
2026-02-04 20:50:25 +0000

    localhost

---
+++ scripts/localhost.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/env bash
+
+echo =========================================================
+echo Running $0 with $# arguments
+echo [processID] $$
+echo [arguments] $@
+echo =========================================================
+
+NVM_VERSION=''
+
+separator_start() {
+  echo '================================================================================= v';
+}
+
+separator_end() {
+  echo '================================================================================= ^';
+}
+
+banner() {
+  separator_start
+  echo $1
+  separator_end
+}
+
+case $1 in
+  git)
+    banner "git fetch --all -p; git pull; git status;"
+    git fetch --all -p; git pull; git status;
+    banner "git merge origin/main;"
+    git merge origin/main;
+    # git push;
+    ;;
+  install)
+    banner "update sdk and use java version in .sdkmanrc"
+    sdk selfupdate
+    sdk env
+    sdk current
+    ;;
+  list)
+    banner "list projects"
+    ./gradlew projects -q
+    ;;
+  *)
+    echo "  git - sync latest from main"
+    echo "  install - update sdk and use java version in .sdkmanrc"
+    ;;
+esac
+
+echo
