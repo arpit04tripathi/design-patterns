56ad84173582dfb93265a9be9ecd7d9342ee428b
Arpit Tripathi <arpit04tripathi@gmail.com>
2026-02-05 19:53:41 +0000

    Main class

--- behavioral/src/main/java/com/a04t/dp/Main.java
+++
@@ -1,7 +0,0 @@
-package com.a04t.dp;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
---
+++ structural/src/main/java/com/a04t/structural/Main.java
@@ -0,0 +1,12 @@
+package com.a04t.structural;
+
+import static com.a04t.dp.common.constants.CommonConstants.DIVIDER;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(DIVIDER);
+        System.out.println("Hello world!");
+        System.out.println(String.format("started %s", Main.class.getCanonicalName()));
+        System.out.println(DIVIDER);
+    }
+}
